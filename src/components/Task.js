fc668617f5eed9ac89355aed878d0fcb4892d03a
Andrew OBrien <aobrienaf@gmail.com>
2023-04-12 16:33:59 -0500

    Working copy 1

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,11 +1,20 @@
 import React from "react";
 
-function Task() {
+function Task( props ) {
+  const { text, category, onDelete} = props;
+
+  const deleteTask = () => {
+    onDelete();
+  };
+
+  
   return (
     <div className="task">
-      <div className="label">CATEGORY HERE</div>
-      <div className="text">TEXT HERE</div>
-      <button className="delete">X</button>
+      <div className="label">{category}</div>
+      <div className="text">{text}</div>
+      <button className="delete" onClick = {deleteTask} >
+        X
+      </button>
     </div>
   );
 }
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,9 +1,41 @@
-import React from "react";
+import React, {useState} from "react";
+import Task from "./Task";
 
-function TaskList() {
+
+
+function TaskList( props ) {
+  const { tasks: initialTasks } = props;
+  const [tasks, setTasks] = useState(initialTasks);
+  
+  const handleDeleteTask = (taskId) => {
+    const updatedTasks = tasks.filter((task) => task !== taskId);
+    setTasks(updatedTasks);
+  }
+  
+  // // set state with tasks
+  // const [taskList, setTaskList] = useState(tasks);
+
+  // // function to remove task from list
+  // function deleteTask(taskId) {
+  //   const updatedTaskList = taskList.filter((task) =>
+  //   task.id !== taskId);
+  //   setTaskList(updatedTaskList);
+  // }
+  // // const [tasks, setTasks] = useState( true )
+  
+  
+  const taskElement = tasks.map((task) => (
+    <Task
+    key = {task.id}
+    text = {task.text}
+    category = {task.category}
+    onDelete = {() => handleDeleteTask(task)}
+    />
+  ));
+
   return (
     <div className="tasks">
-      {/* display a list of tasks using Task component */}
+      {taskElement}
     </div>
   );
 }

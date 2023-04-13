48c5b1e2364fc021ab8cec9bf04e9f889e37436a
Andrew OBrien <aobrienaf@gmail.com>
2023-04-13 08:49:08 -0500

    working copy 2

--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,12 +1,31 @@
-import React from "react";
+import React, {useState} from "react";
 
-function CategoryFilter() {
+function CategoryFilter(props) {
+  const { categories, selectedCategory, onCategoryChange } =props;
+
+  const handleCategoryClick = (category) => {
+    onCategoryChange(category);
+  }
+
+  function handleCategoryClick ( event ) {
+    setSelectedCategory( event.target.value )
+  }
+
   return (
     <div className="categories">
       <h5>Category filters</h5>
-      {/* render <button> elements for each category here */}
+      {categories.map((category) => (
+      <button
+        key = {category}
+        className = {category === selectedCategory}
+        onClick = {() => handleCategoryClick(category)}
+        >
+          {category}
+        </button>  
+      ))}
     </div>
   );
 }
 
 export default CategoryFilter;
+//stopped at when a button is clicked to change category 
